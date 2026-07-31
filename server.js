122e182a16650dfa9d84eb74af960fe24cef8366
shaniaesguerra <shaniae47@gmail.com>
2026-07-31 17:57:04 -0600

    add Swagger UI integration

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const express = require('express');
 const connectDB = require('./config/db');
 const eventsRouter = require('./routes/events');
 
+//Swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//----------- MIDDLEWARE ----------- 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//----------- Swagger UI Route -----------
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//----------- API Routes ----------- 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to EventNexus API, Explore our best Events!  ' });
 });
